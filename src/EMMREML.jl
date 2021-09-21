ae7d3ddebdff15b4a2ff173cf4798e1bfaaff3c9
urchgene <ugo3@cornell.edu>
2021-09-21 12:01:25 -0500

    Included Hmat functions

--- src/EMMREML.jl
+++ src/EMMREML.jl
@@ -9,11 +9,13 @@ include("emmremlJulia.jl")
 include("emmremlMultivariate_Varcomp.jl")
 include("makeGRM.jl")
 include("makeRKHS.jl")
+include("calcHmat.jl")
 #include("emmreml_v2.jl") ## uncomment for using SuperLU in julia >=1.6 
 
 export emmreml, emmremlMultivariate
 export GRM, GRMinv, RKHS, RKHSinv, SqEuclid
 export GRMwted, GRMwtedinv, GRMiter, GRMVariter, DOM
 #export emmreml_LU
+export Hmat
 
 end # module
